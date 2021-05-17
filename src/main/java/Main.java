0d3fc94b9f580dd9f8f651e571e2f8b403ea5792
tagirqa <Smatrix2005@yandex.ru>
2021-05-17 21:22:33 +0300

    Init -> Main edit pom add folder 'lib'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class Init {
+public class Main {
     public static void main(String[] args) {
         ApiContextInitializer.init();
 
